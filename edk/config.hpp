a52c6aefd8306b02763873cd28c023a3cfe319ab
Bryce Lelbach (wash) <blelbach@cct.lsu.edu>
2011-04-25 16:00:40 -0500

    Added central config header, fixed const bug with heapless_pool<>::end, added a static heapless_pool<>::begin (for the owns method).

---
+++ edk/config.hpp
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2011 Bryce Lelbach
+//
+//  Distributed under the Boost Software License, Version 1.0. (See accompanying
+//  file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
+////////////////////////////////////////////////////////////////////////////////
+
+#if !defined(EDK_7F578F96_35D0_4D8F_B5D7_4F131BF74058)
+#define EDK_7F578F96_35D0_4D8F_B5D7_4F131BF74058
+
+#include <edk/config/branch_hints.hpp>
+#include <edk/config/page_size.hpp>
+
+// Defining this macro will allow polymorphic types to be used with edk
+// allocators.
+// #define EDK_NO_POLYMORPHIC_PROTECTION
+
+#endif // EDK_7F578F96_35D0_4D8F_B5D7_4F131BF74058
+
