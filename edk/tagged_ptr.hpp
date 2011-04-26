78ebaeceeab60fe8c51dbae89b5bd2643fb50696
Bryce Lelbach (wash) <blelbach@cct.lsu.edu>
2011-04-26 08:47:48 -0500

    Added tagged_ptr test.

--- edk/tagged_ptr.hpp
+++ edk/tagged_ptr.hpp
@@ -1,6 +1,6 @@
 ////////////////////////////////////////////////////////////////////////////////
-//  Copyright (C) 2008, 2009 Tim Blechmann, based on code by Cory Nelson
-//  Copyright (C) 2011 Bryce Lelbach
+//  Copyright (c) 2008-2009 Tim Blechmann, based on code by Cory Nelson
+//  Copyright (c)      2011 Bryce Lelbach
 //
 //  Distributed under the Boost Software License, Version 1.0. (See accompanying
 //  file BOOST_LICENSE_1_0.rst or copy at http://www.boost.org/LICENSE_1_0.txt)
@@ -21,7 +21,7 @@
 
   namespace edk {
 
-  template <template T>
+  template <typename T>
   struct tagged_ptr {
     typedef T value_type;
     typedef boost::uint64_t compressed_ptr_type;
